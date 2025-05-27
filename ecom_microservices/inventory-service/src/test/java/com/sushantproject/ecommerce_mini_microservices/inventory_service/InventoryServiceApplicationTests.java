405c4dc6d4c7b2a5d18f8a3271952a1c53481e27
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-05-27 18:44:47 +0545

    starting a mini microservices project

---
+++ ecom_microservices/inventory-service/src/test/java/com/sushantproject/ecommerce_mini_microservices/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushantproject.ecommerce_mini_microservices.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ecom_microservices/order-service/src/test/java/com/sushantproject/ecommerce_mini_microservices/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushantproject.ecommerce_mini_microservices.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
